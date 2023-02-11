6c23c6b30535007a246ef9a6b40ddece44c49c18
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2023-02-11 16:02:57 +0700

    starter pack

---
+++ starter-bots/ReferenceBot/ReferenceBot/Enums/ObjectTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace NETCoreBot.Enums
+{
+    public enum ObjectTypes
+    {
+        Player = 1,
+        Food = 2,
+        Wormhole = 3,
+        GasCloud = 4,
+        AsteroidField = 5,
+        TorpedoSalvo = 6,
+        Superfood = 7,
+        SupernovaPickup = 8,
+        SupernovaBomb = 9,
+        Teleporter = 10,
+        Shield = 11
+    }
+}
---
+++ starter-bots/ReferenceBot/ReferenceBot/Enums/PlayerActions.cs
@@ -0,0 +1,16 @@
+﻿namespace NETCoreBot.Enums
+{
+    public enum PlayerActions
+    {
+        Forward = 1,
+        Stop = 2,
+        StartAfterburner = 3,
+        StopAfterburner = 4,
+        FireTorpedoes = 5,
+        FireSupernova = 6,
+        DetonateSupernova = 7,
+        FireTeleport = 8,
+        Teleport = 9,
+        ActivateShield = 10
+    }
+}
