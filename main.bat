b007a538e028dbdadde35aa056d4be1fe516db91
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2023-02-11 16:47:46 +0700

    starter pack

---
+++ main.bat
@@ -0,0 +1,28 @@
+@echo off
+:: Game Runner
+cd ./runner-publish/
+start "" dotnet GameRunner.dll
+
+:: Game Engine
+cd ../engine-publish/
+timeout /t 1
+start "" dotnet Engine.dll
+
+:: Game Logger
+cd ../logger-publish/
+timeout /t 1
+start "" dotnet Logger.dll
+
+:: Bots
+cd ../reference-bot-publish/
+timeout /t 3
+start "" dotnet ReferenceBot.dll
+timeout /t 3
+start "" dotnet ReferenceBot.dll
+timeout /t 3
+start "" dotnet ReferenceBot.dll
+timeout /t 3
+start "" dotnet ReferenceBot.dll
+cd ../
+
+pause
